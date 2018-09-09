962708da244bbf9d0201ee020f1ab970a271680e
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-10 00:38:29 +0800

    url, views, templates for question

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
+
+def show(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/show.html', context)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
