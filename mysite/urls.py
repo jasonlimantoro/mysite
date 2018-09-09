b6304304de454f238174b48c01e32572ffaf80ce
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-10 00:38:39 +0800

    api for question model

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('polls/', include('polls.urls')),
+    path('api/polls/', include('polls.api.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
