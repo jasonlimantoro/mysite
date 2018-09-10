57386fad58bfc58358c381db0c9e57ea68033fb6
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-10 19:43:49 +0800

    add blog and category model

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Question, Choice
+from django.contrib.auth.models import Group
+from .models import Question, Choice, Blog, Category
+
+# Unregister unecessary models
+admin.site.unregister(Group)
+
+class BlogAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category)
+
+admin.site.site_header = "My coolest blog"
+admin.site.index_title = "Models"
+
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -11,7 +12,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,3 +22,25 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    title = models.TextField(max_length=200)
+    description = models.TextField(max_length=200)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+class Blog(models.Model):
+    title = models.TextField(max_length=200)
+    description = models.TextField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return self.title
