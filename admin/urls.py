97d750804b6f6cc52173178eb488bc2b7313896e
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-11 12:36:39 +0800

    view for categories

--- admin/urls.py
+++ admin/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from .categories import views as category_view
 
 app_name = 'admin'
 urlpatterns = [
-    path('', views.index, name='admin'),
+    path('', views.index, name='index'),
+    path('categories', category_view.index, name='categories.index'),
+    path('categories/<int:category_id>/show', category_view.show, name='categories.show'),
+    path('categories/<int:category_id>', category_view.edit, name='categories.edit')
 ]
