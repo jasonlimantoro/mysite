63b6fa297a6ebd8f76219c5e561a7dbed1ee14e3
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-12 21:48:07 +0800

    Change form submission in JavaScript

--- admin/views/like_view.py
+++ admin/views/like_view.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.shortcuts import reverse
 
 
+@require_http_methods(['POST'])
 def store(request, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     Like.objects.create(user=request.user, blog=blog)
@@ -12,6 +13,7 @@ def store(request, blog_id):
     return HttpResponseRedirect(reverse('admin:blogs.show', args=(blog_id,)))
 
 
+@require_http_methods(['POST'])
 def destroy(request, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     blog.like_set.filter(user=request.user).delete()
