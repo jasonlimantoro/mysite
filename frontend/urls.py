3522091d07c7168f0daa2cfd86840fed52540227
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-14 22:31:03 +0800

    User can signup from frontend

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='frontend.home'),
+    path('register', views.register, name='register'),
+    path('signup', views.signup, name='signup'),
     path('categories/<int:id>', views.show_category, name='frontend.categories.show')
 ]
 
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Blog, Category
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from polls.models import Blog, Category, Profile
+from admin.forms import UserForm
 
 
 def home(request):
@@ -27,3 +30,26 @@ def show_category(request, id):
         'blogs': blogs,
         'query': query,
     })
+
+
+def register(request):
+    form = UserForm()
+    return render(request, 'frontend/pages/registration.html', {'form': form})
+
+
+def signup(request):
+    form = UserForm(request.POST)
+    if form.is_valid():
+        user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            email=form.cleaned_data['email'],
+            password=form.cleaned_data['password']
+        )
+        Profile.objects.create(user=user)
+        login(request, user)
+        messages.success(request, "Welcome to the administration board!")
+    else:
+        messages.error(request, "Form is invalid!")
+        return render(request, 'frontend/pages/registration.html', {'form': form})
+
+    return redirect('admin:index')
