de50764f16935531c106bd71d2c17f904783bd6d
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-14 21:15:25 +0800

    fix frontend

--- frontend/views.py
+++ frontend/views.py
@@ -4,14 +4,26 @@ from polls.models import Blog, Category
 
 
 def home(request):
+    query = request.GET.get('query')
     blogs = Blog.objects.all()
+    if query:
+        blogs = blogs.filter(title__icontains=query)
+
     return render(request, 'frontend/home.html', {
         'blogs': blogs,
+        'query': query,
     })
 
 
 def show_category(request, id):
     category_to_show = Category.objects.get(pk=id)
+    query = request.GET.get('query')
+    blogs = category_to_show.blogs.all()
+    if query:
+        blogs = blogs.filter(title__icontains=query)
+
     return render(request, 'frontend/categories/show.html', {
         'category_to_show': category_to_show,
+        'blogs': blogs,
+        'query': query,
     })
