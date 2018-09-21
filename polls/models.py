42817225bc4dd760ed256bbeb946bbe6bfe67a2c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-09-22 07:10:28 +0800

    delete unused models

--- polls/models.py
+++ polls/models.py
@@ -1,30 +1,8 @@
-import os
-import datetime
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
-    
 class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
@@ -35,7 +13,8 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
